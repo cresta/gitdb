0ddd13174ed0056a01f90bb9deb083c3a829d89b
Jack Lindamood <cep221@gmail.com>
2020-09-21 11:07:11 -0700

    With builder.dockerfile

---
+++ builder.dockerfile
@@ -0,0 +1,33 @@
+FROM golang:1.15.0-buster AS builder
+# Install git.
+# Git is required for fetching the dependencies.
+# RUN apt-get update && \
+# 	apt-get install -y --no-install-recommends \
+# 		ca-certificates=20* \
+# 		gcc=4:8* \
+#        		git=1:2* \
+# 		libc-dev  \
+# 		make=4.* \
+# 	       	tzdata && \
+# 	rm -rf /var/lib/apt/lists/*
+WORKDIR /build
+# Create appuser
+ENV USER=appuser
+ENV UID=10001
+RUN groupadd -r --gid ${UID} ${USER} && useradd --uid ${UID} -m --no-log-init -g ${USER} ${USER}
+
+# Install golangci-lint
+ARG GOLANGCI_LINT_VERSION=1.24.0
+RUN curl -L -o /tmp/golangci.tar.gz "https://github.com/golangci/golangci-lint/releases/download/v${GOLANGCI_LINT_VERSION}/golangci-lint-${GOLANGCI_LINT_VERSION}-linux-amd64.tar.gz" && \
+	tar -C /tmp -zxvf /tmp/golangci.tar.gz && \
+	mv /tmp/golangci-lint*/golangci-lint /usr/local/bin
+
+# Install shellcheck
+ARG SHELLCHECK_VERSION=v0.7.1
+RUN curl -L -o /tmp/shellcheck.tar.xz "https://github.com/koalaman/shellcheck/releases/download/${SHELLCHECK_VERSION}/shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz" && \
+	tar -C /tmp -xJv < /tmp/shellcheck.tar.xz && \
+	mv /tmp/shellcheck-${SHELLCHECK_VERSION}/shellcheck /usr/local/bin
+
+# Install hadolint
+ARG HADOLINT_VERSION=v1.17.5
+RUN curl -L -o /usr/local/bin/hadolint "https://github.com/hadolint/hadolint/releases/download/${HADOLINT_VERSION}/hadolint-Linux-x86_64" && chmod a+x /usr/local/bin/hadolint
---
+++ make.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set -ue -o pipefail
+if [ "${DEBUG-}" == "true" ]; then
+  set -x
+fi
+# Repo is part of the image name for this build (repo=repository)
+REPO=${CIRCLE_PROJECT_USERNAME-cresta}/${CIRCLE_PROJECT_REPONAME-terraform}
+# Tag is the image tag of this build's docker file
+TAG=${GIT_COMMIT-$(git rev-parse --verify HEAD)}
+# The docker image is the repository and tag together
+IMAGE=${REPO}:${TAG}
+
+# App is the name of the docker container we execute in dockerrun
+APP=check-${CIRCLE_PROJECT_REPONAME-app}
+# Volume is how we pass local context to the build container.
+VOLUME=mount-${CIRCLE_PROJECT_REPONAME-default}
+
+function build_builder() {
+  docker build -t "${IMAGE}" .
+}
+
+function dockerrun() {
+  (
+    docker rm "${VOLUME}" || true
+    docker rm "${APP}" || true
+  ) 1> /tmp/stdout 2> /tmp/stderr
+  docker create -v /work --name "${VOLUME}" "${IMAGE}" /bin/true >> /tmp/stdout
+  docker cp ./ "${VOLUME}:/work"
+  # Volume trickery to get around mounted volumes not being usable in circleci docker worker
+  docker run -e DEBUG --rm --name "${APP}" --volumes-from "${VOLUME}" "${IMAGE}" "$@"
+}
+
+function test() {
+  env "GORACE=halt_on_error=1" go test -v -benchtime 1ns -bench . -race ./...
+}
+
+function build() {
+  go build -mod=readonly ./...
+}
+
+function reformat() {
+  gofmt -s -w ./..
+	find . -iname '*.go' -print0 | xargs -0 goimports -w
+}
+
+function lint() {
+  golangci-lint run
+  shellcheck ./make.sh
+  hadolint ./Dockerfile
+}
+
+declare -a funcs=(reformat check_formatting export_docker import_docker build_docker dockerrun lint build_builder)
+for f in "${funcs[@]}"; do
+  if [ "${f}" == "${1-}" ]; then
+    $f "${@:2}"
+    exit $?
+  fi
+done
+echo "Invalid param ${1-}.  Valid options: ${funcs[*]}"
+exit 1
