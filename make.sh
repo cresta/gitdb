b35a6f51932eb30b2371936a72df5c16cea67528
Jack Lindamood <jack@cresta.ai>
2020-09-24 11:02:37 -0700

    try out new push (#4)
    * try out new push
    
    * fix container registry
    
    * also add latest tag

--- make.sh
+++ make.sh
@@ -5,12 +5,13 @@ if [ "${DEBUG-}" == "true" ]; then
   set -x
 fi
 
+CONTAINER_REGISTRY="ghcr.io"
 # Repo is part of the image name for this build (repo=repository)
 REPO=${GITHUB_REPOSITORY-unset}
 # Tag is the image tag of this build's docker file
 TAG=${TAG-${GITHUB_SHA-latest}}
 # The docker image is the repository and tag together
-IMAGE=${IMAGE-"${REPO}:${TAG}"}
+IMAGE=${IMAGE-"${CONTAINER_REGISTRY}/${REPO}:${TAG}"}
 BUILDER_IMAGE=builder-gitdb:${TAG}-builder
 
 # App is the name of the docker container we execute in dockerrun
@@ -59,6 +60,25 @@ function reformat() {
 	find . -iname '*.go' -print0 | xargs -0 goimports -w
 }
 
+function docker_tags() {
+  if [[ ${GITHUB_REF} =~ refs/tags/ ]]; then
+    tag=${GITHUB_REF/refs\/tags\//}
+    if [[ ${tag} == v* ]]; then
+      tag=${tag:1}
+    fi
+    echo "${CONTAINER_REGISTRY}/${GITHUB_REPOSITORY}:${tag}"
+  fi
+  if [[ ${GITHUB_REF} =~ refs/heads/ ]]; then
+    tag=${GITHUB_REF/refs\/heads\//}
+    tag=${tag//\//-}
+    echo "${CONTAINER_REGISTRY}/${GITHUB_REPOSITORY}:b-${tag}-$(date -u +"%Y%m%dT%H%M%SZ")-$(echo "${GITHUB_SHA}" | cut -c -7)"
+    echo "${CONTAINER_REGISTRY}/${GITHUB_REPOSITORY}:b-${tag}"
+  fi
+  if [[ ${GITHUB_REF} == refs/heads/master ]]; then
+    echo "${CONTAINER_REGISTRY}/${GITHUB_REPOSITORY}:latest"
+  fi
+}
+
 function lint() {
   golangci-lint run
   shellcheck ./make.sh
@@ -66,7 +86,17 @@ function lint() {
   hadolint ./builder.dockerfile
 }
 
-declare -a funcs=(reformat check_formatting export_docker import_docker build_docker dockerrun lint build_builder build test)
+function push_images() {
+  docker push "${IMAGE}"
+  IFS=$'\n'       # make newlines the only separator
+  for TAG in $(docker_tags); do
+    echo "Making tag ${TAG}"
+    docker tag "${IMAGE}" "${TAG}"
+    docker push "${TAG}"
+  done
+}
+
+declare -a funcs=(reformat check_formatting export_docker import_docker build_docker dockerrun lint build_builder build test docker_tags push_images)
 for f in "${funcs[@]}"; do
   if [ "${f}" == "${1-}" ]; then
     $f "${@:2}"
